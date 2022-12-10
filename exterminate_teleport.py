b83a9f789b4736cd03d41105dce3af373cd222f8
bebebe <113546377+LitvinenkoMaria@users.noreply.github.com>
2022-12-10 22:41:32 +0500

    немного доделала портал

--- exterminate_teleport.py
+++ exterminate_teleport.py
@@ -1,10 +1,14 @@
 from exterminate_colors import *
 from pygame.draw import *
 from math import pi
-
+import random
 import pygame
 import time
 from pygame import draw
+from exterminate_colors import *
 
 class Teleport:
-    
+    def __init__(self, screen, tel_x, tel_y):
+        self.x = tel_x
+        self.y = tel_y
+        self.screen = screen
