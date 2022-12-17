b72d3fe32d4f68dc152878672b4e41bac3fab391
bebebe <113546377+LitvinenkoMaria@users.noreply.github.com>
2022-12-17 18:18:23 +0500

    Теперь можно вручную менять количество уровней
    Появилась функция new_level(), которую можно вызвать столько раз, сколько зададим в переменной level_number

--- main_exterminate.py
+++ main_exterminate.py
@@ -21,16 +21,15 @@ left_key_down = False
 right_key_down = False
 
 
-def display_score():
+def display_score(score):
     """ Отображает текущий счёт."""
-    global score, all_time, level
+    global all_time, level
     all_time += 0.03
     font = pygame.font.SysFont('Comic Sans MS', 26)
     text = font.render('LEVEL ' + str(int(level)) + '', False, WHITE)
     textpos = text.get_rect(centerx=110, y=25)
     screen.blit(text, textpos)
     if int(score) >= 1:
-        score -= 0.03
         text2 = font.render('Time till portal: ' + str(int(score)) + '', False, WHITE)
 
     else:
@@ -57,196 +56,128 @@ def display_results():
     textpos = text.get_rect(centerx=WIDTH / 2, y=HEIGHT / 2)
     screen.blit(text, textpos)
 
+def coordinates_teleport(tard):
+    """
+    Координаты портала задаем взависимости от того, где находится тардис
+    """
+    tel = random.randint(100, 500)
+    if abs(tel - tard) <= 65:
+        tel = 500 - tard
+    return tel
+
+def new_level(tardis, finished, number_l_daleks, number_r_daleks, tel_x, tel_y):
+    """
+    Новый уровень
+    """
+    score = 6
+    left_daleks = []
+    right_daleks = []
+    x_left_dalek = 100
+    x_right_dalek = 750
+    for i in range(number_l_daleks):
+        dalek = Dalek(screen, x_left_dalek)
+        left_daleks.append(dalek)
+    for i in range(number_r_daleks):
+        dalek = Dalek2(screen, x_right_dalek)
+        right_daleks.append(dalek)
+    while not finished:
+        if not tardis.alive:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    finished = True
+            display_results()
+            pygame.display.update()
+        else:
+            score -= 0.03
+            space = pygame.image.load('SpaceBackGround.bmp')
+            screen.blit(space, (0, 0))
+            rect(screen, WHITE, (130, 5, 540, 595), 2)
+            tardis.move()
+            tardis.draw()
+            display_score(score)
+            if int(score) <= 0:
+                tel_image = pygame.image.load('portal5.jpg')
+                tel_image = pygame.transform.scale(tel_image, (120, 120))
+                tel_image.set_colorkey(BLACK)
+                screen.blit(tel_image, (tel_x, tel_y))
+                tardis.draw()
+                pygame.display.update()
+                print("ahaha")
+                if (0 <= (tardis.x - teleport.x) <= 70) and (0 <= (tardis.y - teleport.y) <= 20):
+                    rect(screen, WHITE, (WIDTH / 2 - 350, HEIGHT / 2 - 40, 700, 100))
+                    rect(screen, BLACK, (WIDTH / 2 - 350, HEIGHT / 2 - 40, 700, 100), 2)
+                    font = pygame.font.SysFont('Verdana', 22)
+                    text = font.render('Congrats. You have survived for ' + str(int(all_time)) + ' seconds.', True,
+                                       BLACK)
+                    textpos = text.get_rect(centerx=WIDTH / 2, y=HEIGHT / 2)
+                    screen.blit(text, textpos)
+                    score = 0
+                    first_level_passed = True
+                    pygame.display.update()
+                    time.sleep(4)
+                    finished = True
+            for dalek in left_daleks:
+                dalek.spawn_bomb(bombs1)
+                dalek.move()
+                dalek.draw()
+            for dalek in right_daleks:
+                dalek.spawn_bomb(bombs2)
+                dalek.move()
+                dalek.draw()
+            for bomb in bombs1:
+                bomb.hit_tardis(tardis)
+                bomb.move_right()
+                bomb.draw()
+            for bomb in bombs2:
+                bomb.hit_tardis(tardis)
+                bomb.move_left()
+                bomb.draw()
+            pygame.display.update()
+
+            clock.tick(FPS)
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        up_key_down = True
+                    if event.key == pygame.K_DOWN:
+                        down_key_down = True
+                    if event.key == pygame.K_RIGHT:
+                        right_key_down = True
+                    if event.key == pygame.K_LEFT:
+                        left_key_down = True
+
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP:
+                        up_key_down = False
+                    if event.key == pygame.K_DOWN:
+                        down_key_down = False
+                    if event.key == pygame.K_RIGHT:
+                        right_key_down = False
+                    if event.key == pygame.K_LEFT:
+                        left_key_down = False
+                if event.type == pygame.QUIT:
+                    finished = True
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-bullet_count = 0
-daleks = []
 
 bombs1 = []
 bombs2 = []
-x_dalek = 100
 clock = pygame.time.Clock()
 
 tardis = Tardis(screen)
-first_level_passed = False
-dalek1 = Dalek(screen, x_dalek)
 
-tel_x = random.randint(100, 500)
-tel_y = random.randint(100, 500)
-if abs(tel_x - tardis.x) <= 65:
-    tel_x = 500 - tardis.x
-if abs(tel_y - tardis.y) <= 30:
-    tel_y = 500 - tardis.y
-
+tel_x = coordinates_teleport(tardis.x)
+tel_y = coordinates_teleport(tardis.y)
 teleport = Teleport(screen, tel_x, tel_y)
 finished = False
 
-score = 6
 all_time = 0
 
-# первый уровень
-while not finished:
-    if not tardis.alive:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                finished = True
-        display_results()
-        pygame.display.update()
-    else:
-        space = pygame.image.load('SpaceBackGround.bmp')
-        screen.blit(space, (0, 0))
-        rect(screen, WHITE, (130, 5, 540, 595), 2)
-        tardis.move()
-        tardis.draw()
-        display_score()
-        if int(score) <= 0:
-            tel_image = pygame.image.load('portal5.jpg')
-            tel_image = pygame.transform.scale(tel_image, (120, 120))
-            tel_image.set_colorkey(BLACK)
-            screen.blit(tel_image, (tel_x, tel_y))
-            tardis.draw()
-            pygame.display.update()
-
-            if (0 <= (tardis.x - teleport.x) <= 70) and (0 <= (tardis.y - teleport.y) <= 20):
-                rect(screen, WHITE, (WIDTH / 2 - 350, HEIGHT / 2 - 40, 700, 100))
-                rect(screen, BLACK, (WIDTH / 2 - 350, HEIGHT / 2 - 40, 700, 100), 2)
-                font = pygame.font.SysFont('Verdana', 22)
-                text = font.render('Congrats. You have survived for ' + str(int(all_time)) + ' seconds.', True,
-                                   BLACK)
-                textpos = text.get_rect(centerx=WIDTH / 2, y=HEIGHT / 2)
-                screen.blit(text, textpos)
-                score = 0
-                first_level_passed = True
-                pygame.display.update()
-                time.sleep(4)
-                finished = True
-        dalek1.spawn_bomb(bombs1)
-        dalek1.move()
-        dalek1.draw()
-        for bomb in bombs1:
-            bomb.hit_tardis(tardis)
-            bomb.move_right()
-            bomb.draw()
-
-        pygame.display.update()
-
-        clock.tick(FPS)
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    up_key_down = True
-                if event.key == pygame.K_DOWN:
-                    down_key_down = True
-                if event.key == pygame.K_RIGHT:
-                    right_key_down = True
-                if event.key == pygame.K_LEFT:
-                    left_key_down = True
-
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_UP:
-                    up_key_down = False
-                if event.key == pygame.K_DOWN:
-                    down_key_down = False
-                if event.key == pygame.K_RIGHT:
-                    right_key_down = False
-                if event.key == pygame.K_LEFT:
-                    left_key_down = False
-            if event.type == pygame.QUIT:
-                finished = True
-
-tardis = Tardis(screen)
-tel_x = random.randint(100, 500)
-tel_y = random.randint(100, 500)
-if abs(tardis.x - tel_x) <= 65:
-    tel_x = 500 - tardis.x
-if abs(tardis.y - tel_y) <= 30:
-    tel_y = 500 - tardis.y
-
-teleport = Teleport(screen, tel_x, tel_y)
-
-level2 = False
-score = 6
-level += 1
-x_dalek = 750
-dalek2 = Dalek2(screen, x_dalek)
-
-while not level2 and first_level_passed:
-    if not tardis.alive:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                level2 = True
-        display_results()
-        pygame.display.update()
-    else:
-        screen.fill(BLACK)
-        space = pygame.image.load('SpaceBackGround.bmp')
-        screen.blit(space, (0, 0))
-        rect(screen, WHITE, (130, 5, 540, 595), 2)
-        tardis.move()
-        tardis.draw()
-        display_score()
-        if int(score) <= 0:
-            tel_image = pygame.image.load('portal5.jpg')
-            tel_image = pygame.transform.scale(tel_image, (120, 120))
-            tel_image.set_colorkey(BLACK)
-            screen.blit(tel_image, (tel_x, tel_y))
-            tardis.draw()
-            pygame.display.update()
-
-            if (0 <= (tardis.x - teleport.x) <= 70) and (0 <= (tardis.y - teleport.y) <= 20):
-                rect(screen, WHITE, (WIDTH / 2 - 350, HEIGHT / 2 - 40, 700, 100))
-                rect(screen, BLACK, (WIDTH / 2 - 350, HEIGHT / 2 - 40, 700, 100), 2)
-                font = pygame.font.SysFont('Verdana', 22)
-                text = font.render('Congrats. You have survived for ' + str(int(all_time)) + ' seconds.', True,
-                                   BLACK)
-                textpos = text.get_rect(centerx=WIDTH / 2, y=HEIGHT / 2)
-                screen.blit(text, textpos)
-                score = 0
-                pygame.display.update()
-                time.sleep(4)
-                level2 = True
-        dalek1.spawn_bomb(bombs1)
-        dalek1.move()
-        dalek1.draw()
-
-        dalek2.spawn_bomb(bombs2)
-        dalek2.move()
-        dalek2.draw()
-
-
-        for bomb in bombs1:
-            bomb.hit_tardis(tardis)
-            bomb.move_right()
-            bomb.draw()
-        for bomb in bombs2:
-            bomb.hit_tardis(tardis)
-            bomb.move_left()
-            bomb.draw()
-        pygame.display.update()
-
-        clock.tick(FPS)
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    up_key_down = True
-                if event.key == pygame.K_DOWN:
-                    down_key_down = True
-                if event.key == pygame.K_RIGHT:
-                    right_key_down = True
-                if event.key == pygame.K_LEFT:
-                    left_key_down = True
-
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_UP:
-                    up_key_down = False
-                if event.key == pygame.K_DOWN:
-                    down_key_down = False
-                if event.key == pygame.K_RIGHT:
-                    right_key_down = False
-                if event.key == pygame.K_LEFT:
-                    left_key_down = False
-            if event.type == pygame.QUIT:
-                level2 = True
-
-pygame.quit()
+level_number = 2 #число уровней
+number_r_daleks = 0
+number_l_daleks = 1
+for i in range(level_number):
+    new_level(tardis, finished, number_l_daleks, number_r_daleks, tel_x, tel_y)
+    number_l_daleks += i % 2
+    number_r_daleks += (i + 1) % 2
