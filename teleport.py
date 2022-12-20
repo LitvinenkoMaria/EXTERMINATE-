133ba80f8a4bb7fb3a4f722a583c6c980f7a6b6a
bebebe <113546377+LitvinenkoMaria@users.noreply.github.com>
2022-12-20 15:35:11 +0500

    Добавлена документация

--- teleport.py
+++ teleport.py
@@ -3,11 +3,22 @@ import pygame
 from colors import BLACK
 
 class Teleport:
+    """
+    Портал нужен для перехода на новый уровень
+    """
     def __init__(self, screen, tel_x, tel_y):
+        """
+        Портал появляется на экране screen;
+        tel_x, tel_y - координаты портала, которые задаются в зависимости
+        от координат ТАРДИС.
+        """
         self.x = tel_x
         self.y = tel_y
         self.screen = screen
     def draw(self, scale_tardis, screen):
+        """
+        Рисует портал.
+        """
         k = scale_tardis
         tel_image = pygame.image.load('portal5.jpg')
         tel_size = 120
